2fe513db2af114f1ed04a9d2b9069944f79bd53a
vvm1004 <vovanminhv23@gmail.com>
2025-07-04 10:08:10 +0700

    add CronJob

--- backend/src/messages/messages.service.ts
+++ backend/src/messages/messages.service.ts
@@ -1,34 +1,47 @@
 import { Injectable } from '@nestjs/common';
-import { CreateMessageDto } from './dto/create-message.dto';
-import { UpdateMessageDto } from './dto/update-message.dto';
 import { InjectModel } from '@nestjs/mongoose';
-import { Message, MessageDocument } from './schemas/message.schema';
+import { ConfigService } from '@nestjs/config';
+import { Cron } from '@nestjs/schedule';
 import { Model } from 'mongoose';
+import axios from 'axios';
+
+import { Message, MessageDocument } from './schemas/message.schema';
+import { CreateMessageDto } from './dto/create-message.dto';
 import { IUser } from 'src/users/users.interface';
 
 @Injectable()
 export class MessagesService {
   constructor(
-      @InjectModel(Message.name) private messageModel: Model<MessageDocument>,
-    ) {}
+    @InjectModel(Message.name) private messageModel: Model<MessageDocument>,
+    private readonly configService: ConfigService,
+  ) {}
 
   async sendMessage(createMessageDto: CreateMessageDto, user: IUser) {
-    const newMessage = new this.messageModel({ ...createMessageDto, senderId: user._id })
-    return newMessage.save()
+    const newMessage = new this.messageModel({ ...createMessageDto, senderId: user._id });
+    return newMessage.save();
   }
 
   async getMessages(senderId: string, receiverId: string) {
-  return this.messageModel
-    .find({
-      $or: [
-        { senderId: senderId, receiverId: receiverId },
-        { senderId: receiverId, receiverId: senderId },
-      ],
-    })
-    .sort({ createdAt: 1 }) 
-    .exec()
-}
+    return this.messageModel
+      .find({
+        $or: [
+          { senderId: senderId, receiverId: receiverId },
+          { senderId: receiverId, receiverId: senderId },
+        ],
+      })
+      .sort({ createdAt: 1 })
+      .exec();
+  }
 
+  @Cron('*/3 * * * *')
+  async handleCronEvery() {
+    const apiUrl = this.configService.get<string>('PING_URL');
 
-  
+    try {
+      const response = await axios.get(apiUrl);
+      console.log('Ping successful:', response.status);
+    } catch (error) {
+      console.error('API Request failed:', error.message);
+    }
+  }
 }
